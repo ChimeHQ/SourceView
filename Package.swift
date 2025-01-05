711c03b1df5432f5c53e5cb111b86e38c53be648
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-01-05 07:34:52 -0500

    Change to main branch

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		.library(name: "SourceView", targets: ["SourceView"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/ChimeHQ/IBeam", revision: "53e0ea98ce3fa299ebbd76bd7b17c561908c763c"),
+		.package(url: "https://github.com/ChimeHQ/IBeam", branch: "main"),
 		.package(url: "https://github.com/ChimeHQ/KeyCodes", from: "1.0.3"),
 		.package(url: "https://github.com/ChimeHQ/Ligature", branch: "main"),
 		.package(url: "https://github.com/ChimeHQ/Textbook", revision: "a70d9cd0b7da1878e1db448283b431834fd00376"),
