9262e00508b63d1da3b88056ff4e9409b6d45e14
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-01-12 06:48:12 -0500

    Fix whitespace

--- Sources/SourceView/MultiCursorTextView.swift
+++ Sources/SourceView/MultiCursorTextView.swift
@@ -13,77 +13,99 @@ extension KeyModifierFlags {
 
 open class MultiCursorTextView: BaseTextView {
 	public var operationProcessor: (InputOperation) -> Bool = { _ in false }
-    public var cursorOperationHandler: (CursorOperation<NSRange>) -> Void = { _ in }
+	public var cursorOperationHandler: (CursorOperation<NSRange>) -> Void = { _ in }
 }
 
 extension MultiCursorTextView {
-    open override func insertText(_ input: Any, replacementRange: NSRange) {
-        // also should handle replacementRange values
+	open override func insertText(_ input: Any, replacementRange: NSRange) {
+		// also should handle replacementRange values
 
-        let attrString: AttributedString
+		let attrString: AttributedString
 
-        switch input {
+		switch input {
 		case let string as String:
-            let container = AttributeContainer(typingAttributes)
+			let container = AttributeContainer(typingAttributes)
 
-            attrString = AttributedString(string, attributes: container)
-        case let string as NSAttributedString:
-            attrString = AttributedString(string)
-        default:
-            fatalError("This API should be called with NSString or NSAttributedString only")
-        }
+			attrString = AttributedString(string, attributes: container)
+		case let string as NSAttributedString:
+			attrString = AttributedString(string)
+		default:
+			fatalError("This API should be called with NSString or NSAttributedString only")
+		}
 
 		if operationProcessor(.insertText(attrString)) {
 			return
 		}
 
 		super.insertText(input, replacementRange: replacementRange)
-    }
+	}
 
-    open override func doCommand(by selector: Selector) {
-        if let op = InputOperation(selector: selector) {
+	open override func doCommand(by selector: Selector) {
+		if let op = InputOperation(selector: selector) {
 			if operationProcessor(op) {
 				return
 			}
-        }
+		}
 
-        super.doCommand(by: selector)
-    }
+		super.doCommand(by: selector)
+	}
 
-    // this enable correct routing for the mouse down
-    open override func menu(for event: NSEvent) -> NSMenu? {
-        if event.keyModifierFlags?.addingCursor == true {
-            return nil
-        }
+	// this enable correct routing for the mouse down
+	open override func menu(for event: NSEvent) -> NSMenu? {
+		if event.keyModifierFlags?.addingCursor == true {
+			return nil
+		}
 
-        return super.menu(for: event)
-    }
+		return super.menu(for: event)
+	}
 
-    open override func mouseDown(with event: NSEvent) {
-        guard event.keyModifierFlags?.addingCursor == true else {
-            super.mouseDown(with: event)
-            return
-        }
+	open override func mouseDown(with event: NSEvent) {
+		guard event.keyModifierFlags?.addingCursor == true else {
+			super.mouseDown(with: event)
+			return
+		}
 
-        let point = convert(event.locationInWindow, from: nil)
-        let index = characterIndexForInsertion(at: point)
-        let range = NSRange(index..<index)
+		let point = convert(event.locationInWindow, from: nil)
+		let index = characterIndexForInsertion(at: point)
+		let range = NSRange(index..<index)
 
-        cursorOperationHandler(.add(range))
-    }
+		cursorOperationHandler(.add(range))
+	}
 
-    open override func keyDown(with event: NSEvent) {
-        let flags = event.keyModifierFlags?.subtracting(.numericPad) ?? []
-        let key = event.keyboardHIDUsage
+	open override func keyDown(with event: NSEvent) {
+		let flags = event.keyModifierFlags?.subtracting(.numericPad) ?? []
+		let key = event.keyboardHIDUsage
 
-        switch (flags, key) {
-        case ([.control, .shift], .keyboardUpArrow):
-            cursorOperationHandler(.addAbove)
-        case ([.control, .shift], .keyboardDownArrow):
-            cursorOperationHandler(.addBelow)
-        default:
-            super.keyDown(with: event)
-        }
-    }
+		switch (flags, key) {
+		case ([.control, .shift], .keyboardUpArrow):
+			cursorOperationHandler(.addAbove)
+		case ([.control, .shift], .keyboardDownArrow):
+			cursorOperationHandler(.addBelow)
+		default:
+			super.keyDown(with: event)
+		}
+	}
+
+	open override func paste(_ sender: Any?) {
+		let pasteboard = NSPasteboard.general
+
+		// this path has not fallback
+		if let stringArray = pasteboard.multipleTextSelectionStrings() {
+			if operationProcessor(.insertTextArray(stringArray)) == false {
+				NSSound.beep()
+			}
+			
+			return
+		}
+
+		// a single string paste is different, because it can apply unconditionally to all cursors
+		if let string = pasteboard.string(forType: .string) {
+			if operationProcessor(.insertText(string)) {
+				return
+			}
+		}
+
+		NSSound.beep()
+	}
 }
 #endif
